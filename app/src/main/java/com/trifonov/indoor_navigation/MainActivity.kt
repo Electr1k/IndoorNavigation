58080ebbd7e7cbb56aa0f0ec0c500a2216f4e106
Electr1k <110545080+Electr1k@users.noreply.github.com>
2023-10-31 22:16:59 +0300

    Added bottom navigation

--- app/src/main/java/com/trifonov/indoor_navigation/MainActivity.kt
+++ app/src/main/java/com/trifonov/indoor_navigation/MainActivity.kt
@@ -2,10 +2,41 @@ package com.trifonov.indoor_navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNav = findViewById(R.id.bottomNavigationView)
+
+
+        bottomNav.setOnItemSelectedListener {
+            it.isEnabled = true
+            when (it.itemId) {
+                R.id.head -> {
+                    println("Главная")
+                    true
+                }
+                R.id.search -> {
+                    println("Поиск")
+                    true
+                }
+                R.id.route -> {
+                    println("Маршрут")
+                    true
+                }
+                R.id.scan -> {
+                    println("Сканер")
+                    true
+                }
+                else -> {
+                    println("Хз")
+                    false
+                }
+            }
+        }
     }
 }
