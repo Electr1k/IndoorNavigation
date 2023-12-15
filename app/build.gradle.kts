b29f9fd9b159399423a0b1ab0951864bda7ac471
Nikolay Trifonov <110545080+Electr1k@users.noreply.github.com>
2023-12-15 17:58:19 +0300

    Added map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xjvm-default=all-compatibility")
     }
     buildFeatures {
         viewBinding = true
@@ -49,6 +50,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("ovh.plrapps:mapview:3.1.8")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/trifonov/indoor_navigation/MainActivity.kt
+++ app/src/main/java/com/trifonov/indoor_navigation/MainActivity.kt
@@ -9,16 +9,36 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.trifonov.indoor_navigation.databinding.ActivityMainBinding
+import ovh.plrapps.mapview.MapView
+import ovh.plrapps.mapview.MapViewConfiguration
+import ovh.plrapps.mapview.core.TileStreamProvider
+
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
     private lateinit var mNavController: NavController
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        mapView = findViewById(R.id.mapView)
+        val tileStreamProvider = TileStreamProvider { row, col, zoomLvl ->
+            try {
+                this.assets?.open("tiles/$zoomLvl/$row/$col.jpg")
+            }
+            catch (e: Exception){
+                this.assets?.open("tiles/blank.png")
+            }
+        }
+        val config = MapViewConfiguration(levelCount = 5, fullWidth = 3840, fullHeight = 2160,
+            tileSize = 256, tileStreamProvider = tileStreamProvider)
+            .enableRotation()
+        mapView.configure(config)
 
         val navView: BottomNavigationView = mBinding.bottomNavigationView
 
@@ -28,5 +48,15 @@ class MainActivity : AppCompatActivity() {
             if (destination.id != R.id.head) findViewById<CardView>(R.id.cardNav).visibility = View.INVISIBLE
             else findViewById<CardView>(R.id.cardNav).visibility = View.VISIBLE
         }
+    }
+
+    fun scalePlus(view: View){
+        println(mapView.scale)
+        mapView.setScaleFromCenter(mapView.scale * 2)
+    }
+
+    fun scaleMinus(view: View){
+        println(mapView.scale)
+        mapView.setScaleFromCenter(mapView.scale / 2)
     }
 }
