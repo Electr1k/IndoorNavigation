234fe941b37194463ad68d52c2cea3e51ef04a6d
Electr1k <gynter37@gmail.com>
2024-01-26 15:48:02 +0300

    remake alert dialog

--- app/src/main/java/com/trifonov/indoor_navigation/MainActivity.kt
+++ app/src/main/java/com/trifonov/indoor_navigation/MainActivity.kt
@@ -8,12 +8,18 @@ import android.os.Handler
 import android.os.Looper
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.button.MaterialButton
+import com.trifonov.indoor_navigation.common.LocationData
+import com.trifonov.indoor_navigation.common.LocationEntity
 import com.trifonov.indoor_navigation.databinding.ActivityMainBinding
+import com.trifonov.indoor_navigation.map.FileHelper.Companion.checkStorageLocation
 import com.trifonov.indoor_navigation.map.MapConstants.mapConnector
 import com.trifonov.indoor_navigation.map.MapConstants.startNode
 
@@ -34,32 +40,32 @@ class MainActivity : AppCompatActivity() {
             if (destination.id != R.id.head) findViewById<CardView>(R.id.cardNav).visibility = View.INVISIBLE
             else findViewById<CardView>(R.id.cardNav).visibility = View.VISIBLE
         }
-        Handler(Looper.getMainLooper()).postDelayed({initialAlertDialog()}, 7000)
+        val locationData = LocationData(this)
+        Handler(Looper.getMainLooper()).postDelayed({initialAlertDialog(locationData.getLocationById(0)!!)}, 2000)
     }
 
     /**
      * Инициализирует и запускает Alert Dialog с загрузкой локации
      * */
-    private fun initialAlertDialog(){
+    internal fun initialAlertDialog(location: LocationEntity){
         val builder = AlertDialog.Builder(this)
-        val dialog = builder.setMessage("Вы находитесь в корпусе Г, хотите загрузить карту локации?")
-            .setPositiveButton("Да", null)
-            .setNegativeButton("Нет"){
-                dialog, id ->  dialog.cancel()
-            }
+        val dialog = builder
             .create()
         dialog.window?.setBackgroundDrawable(getDrawable(R.drawable.dialog_rounded_background))
         dialog.setCanceledOnTouchOutside(false)
-        dialog.show()
-        val positive_btn = dialog.getButton(DialogInterface.BUTTON_POSITIVE)
-        positive_btn.setTextColor(getColor(R.color.dark_blue))
-        positive_btn.setOnClickListener{
-            val alertDialogView = dialog.window!!.decorView
+        val alertDialogView = dialog.window!!.decorView
+        val confirmView = layoutInflater.inflate(R.layout.confirm_download, null)
+        val viewGroup = alertDialogView as ViewGroup
+        val isDownload = checkStorageLocation(location.dataUrl)
+        confirmView.findViewById<TextView>(R.id.confirmText).text = if (isDownload) "Вы находитесь в ${location.name}, хотите переключиться на карту локации?" else "Карта локации ${location.name} не установлена на Ваше устройство, выполнить её загрузку?"
+        confirmView.findViewById<Button>(R.id.positiveBtn).setOnClickListener{
             val downloadView = layoutInflater.inflate(R.layout.download_view, null)
-            val viewGroup = alertDialogView as ViewGroup
-            viewGroup.addView(downloadView)
+            if (!isDownload){
+                viewGroup.removeView(confirmView)
+                viewGroup.addView(downloadView)
+            }
             Thread {
-                if (mapConnector.initialMapView("Korpus_G", downloadView, dialog)) {
+                if (mapConnector.initialMapView(location.dataUrl, downloadView, dialog)) {
                     startNode++
                     this.runOnUiThread {
                         mapConnector.updatePath(136)
@@ -68,8 +74,11 @@ class MainActivity : AppCompatActivity() {
                 }
             }.start()
         }
-        val negative_btn = dialog.getButton(DialogInterface.BUTTON_NEGATIVE)
-        negative_btn.setTextColor(getColor(R.color.dark_blue))
+        confirmView.findViewById<Button>(R.id.negativeBtn).setOnClickListener {
+            dialog.cancel()
+        }
+        viewGroup.addView(confirmView)
+        dialog.show()
     }
 
 }
