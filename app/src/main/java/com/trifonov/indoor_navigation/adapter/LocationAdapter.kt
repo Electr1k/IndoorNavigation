fa82d781d6acb7b4b998b52a64e526204acbe4a4
Electr1k <gynter37@gmail.com>
2024-02-28 19:02:58 +0300

    added downloaded icon for location

--- app/src/main/java/com/trifonov/indoor_navigation/adapter/LocationAdapter.kt
+++ app/src/main/java/com/trifonov/indoor_navigation/adapter/LocationAdapter.kt
@@ -3,12 +3,14 @@ package com.trifonov.indoor_navigation.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.trifonov.indoor_navigation.R
 import com.trifonov.indoor_navigation.common.LocationEntity
 import com.trifonov.indoor_navigation.data.dto.Location
+import com.trifonov.indoor_navigation.map.FileHelper.Companion.checkStorageLocation
 
 class LocationAdapter(
     private val locationList: List<Location>,
@@ -31,6 +33,7 @@ class LocationAdapter(
     override fun onBindViewHolder(holder: LocationViewHolder, position: Int) {
         holder.title.text = locationList[position].name
         holder.address.text = locationList[position].address
+        holder.downloadedIcon.visibility = if (checkStorageLocation(locationList[position].dataUrl)) View.VISIBLE else View.INVISIBLE
         holder.btn.setOnClickListener{
             selectedLocation = locationList[position]
             click(selectedLocation!!)
@@ -46,10 +49,12 @@ class LocationAdapter(
         val title: TextView
         val address: TextView
         val btn: RadioButton
+        val downloadedIcon: ImageView
         init {
             title = itemView.findViewById(R.id.title)
             address = itemView.findViewById(R.id.address)
             btn = itemView.findViewById(R.id.btn)
+            downloadedIcon = itemView.findViewById(R.id.download_icon)
         }
     }
 }
